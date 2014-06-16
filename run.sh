778e14bb1b9f49f9f23ff17d917d164734101725
Shoichi Takagi <fly1tkg@gmail.com>
2014-06-16 16:07:07 +0900

    fix environment variables, update version

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 
-echo no | android create avd --force -n $WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME -t $WERCKER_SETUP_ANDROID_EMULATOR_TARGET --abi $WERCKER_SETUP_ANDROID_EMULATOR_ABI -c 256M
-echo "hw.keyboard=yes" >> ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME.avd/config.ini
+echo no | android create avd --force -n $WERCKER_SETUP_ANDROID_EMULATOR_WITH_SDCARD_TARGET_NAME -t $WERCKER_SETUP_ANDROID_EMULATOR_WITH_SDCARD_TARGET --abi $WERCKER_SETUP_ANDROID_EMULATOR_WITH_SDCARD_ABI -c 256M
+echo "hw.keyboard=yes" >> ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_WITH_SDCARD_TARGET_NAME.avd/config.ini
 echo "Enabled hardware keyboard"
-cat ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME.avd/config.ini
-emulator -avd $WERCKER_SETUP_ANDROID_EMULATOR_TARGET_NAME -no-audio -no-window &
+cat ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_WITH_SDCARD_TARGET_NAME.avd/config.ini
+emulator -avd $WERCKER_SETUP_ANDROID_EMULATOR_WITH_SDCARD_TARGET_NAME -no-audio -no-window &
 $WERCKER_STEP_ROOT/wait_for_emulator.sh
