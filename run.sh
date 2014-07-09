d6a1b5d19acda7af0c7294e417a0ec45a1a8f858
Shoichi Takagi <fly1tkg@gmail.com>
2014-07-09 10:34:19 +0900

    remove `-verbose` option and `adb reboot`

--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@ echo no | android create avd --force -n $WERCKER_SETUP_ANDROID_EMULATOR_WITH_SDC
 echo "hw.keyboard=yes" >> ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_WITH_SDCARD_TARGET_NAME.avd/config.ini
 echo "Enabled hardware keyboard"
 cat ~/.android/avd/$WERCKER_SETUP_ANDROID_EMULATOR_WITH_SDCARD_TARGET_NAME.avd/config.ini
-adb reboot
-emulator -avd $WERCKER_SETUP_ANDROID_EMULATOR_WITH_SDCARD_TARGET_NAME -no-audio -no-window -verbose &
+emulator -avd $WERCKER_SETUP_ANDROID_EMULATOR_WITH_SDCARD_TARGET_NAME -no-audio -no-window &
 $WERCKER_STEP_ROOT/wait_for_emulator.sh
